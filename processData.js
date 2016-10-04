b49546020a1d7b007f8f693ec816d5b035327aa7
abelsan <abel@mit.edu>
2016-10-04 11:27:00 -0400

    Added Starter Code

---
+++ processData.js
@@ -0,0 +1,4 @@
+var json2csv = require('json2csv');
+var fs = require('fs');
+
+var fields = [ 'sid','id','position','created_at','created_meta','updated_at','updated_meta','meta','name','title','department_name','regular','retro','other','overtime','injured','detail','quinn','total_earnings','zip'];
